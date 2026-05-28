810b6190c6d652ea1dc3791cdb6323392def52ba
livekuhni <livekuhni@gmail.com>
2026-05-28 15:46:44 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,8 +39,8 @@ async def edit_with_gpt4o(image_bytes: bytearray) -> bytes:
                 "model": "gpt-image-1",
                 "prompt": EDIT_PROMPT,
                 "n": "1",
-                "size": "1024x1024",
-                "quality": "medium"
+                "size": "1536x1024",
+                "quality": "high"
             }
         )
         data = response.json()
